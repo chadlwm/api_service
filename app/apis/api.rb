03e97b23da7761e47a7b942018757b5413e61baf
liuwm <liuwm@grandsoft.com.cn>
2013-12-09 02:52:24 +0800

    insert to db

--- app/apis/api.rb
+++ app/apis/api.rb
@@ -11,14 +11,22 @@ module MagicT
 
     desc "record the log data with post"
     post :do do
-      p params[:id]
       p params
-      {'name' => 'chad', 'description' => 'this is public time line.'}
+      log = Log.new
+      log.remote_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.ip
+      log.create_info(JSON.parse(params["do_data"]))
+      if(log.save)
+        ret = {'result' => 'success', 'result-code' => 200}
+      else
+        ret = {'result' => 'failed', 'result-code' => 500}
+      end
+      #{'name' => 'chad', 'description' => 'this is public time line.'}
     end
 
     desc "test get data from db"
     get :info do
-     {'result' => 'success'}
+      a = {'result' => 'success'}
+      {'rest' => a.to_s}
     end
   end
 end
